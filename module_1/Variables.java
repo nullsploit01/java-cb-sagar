849e9783f226851ee515929cdd33c61ebb5d6a3f
nullsploit01 <harshaldharmik099@gmail.com>
2025-08-08 07:32:33 +0530

    java day 1

---
+++ module_1/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 45;
+        boolean likesJava = false;
+
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println("Does he likes java? " + likesJava);
+    }
+}
